c46869b204b4add2c782863aeb9b3212aa40e0d3
Zura Kvachakidze <zura1308@gmail.com>
2020-10-20 21:05:59 +0400

    Remastered

--- note.js
+++ note.js
@@ -5,26 +5,70 @@ const newNoteBtn=document.getElementById("new-btn");
 const saveBtn=document.getElementById("save-btn");
 const time=document.getElementById("date");
 
-
-let saveText = document.createElement("p")
 let noteArray=[];  
 let myNote = null;
+let id=0;
+let curentTargetId = null;
 
 
-time.textContent=Date().substring(0, 25);
+time.innerHTML=Date().substring(0, 25);
 
 
-function save () {
-    saveText.setAttribute('saveText');
-    noteSaveSection.appendChild(saveText);
-    saveText.textContent = textarea.value;
+function deleteElement(){
+   const element=document.getElementById(curentTargetId);
+   element.remove();
+   curentTargetId = null;
+}
+
+
+function editNote(event){
+    curentTargetId=event.currentTarget.id;
+    areaContent.value = event.currentTarget.children[1].textContent;
+}
+
+
+// function myTimer() {
+//     var d = new Date();
+//     var t = d.toLocaleTimeString();
+//     const saveText = document.createElement("p"); 
+
+//     const span = document.createElement("span");
+//     saveText.appendChild(title);
+//     saveText.appendChild(saveText);
+//     document.getElementById("span").innerHTML = t;
+//   }
+  
+
+function save() {
+    if(curentTargetId !== null){
+        deleteElement();
+    }
+
+    let line = areaContent.value.split('\n')[0];
+    let title = document.createElement("h2");
+    title.innerHTML=line;
+
+    const saveText = document.createElement("p"); 
+
+    const liElement=document.createElement("li");
+    liElement.appendChild(title);
+    liElement.appendChild(saveText);
+    
+    noteSaveSection.appendChild(liElement);
+    saveText.textContent = areaContent.value;
+    liElement.setAttribute("id", id);
+    id++;
+    liElement.addEventListener("click", editNote);
+    areaContent.value = "";
 }
 
 
 newNoteBtn.addEventListener('click', function(){
-    areaContent.style.display = "none";
+    areaContent.style.display = "block"; 
+    areaContent.value=""; 
+    curentTargetId = null;
 });
 
 
 saveBtn.addEventListener("click", save);
-
+deleteBtn.addEventListener("click", deleteElement);
